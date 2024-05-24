73156d4f225a6f124f116b9e1b9c20bd837a07ad
angelika1108 <angelika11085004@gmail.com>
2024-05-24 17:08:16 +0200

    readme and small changes

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# pixelsplat_fisheye
-PixelSplat for fisheye images
+# PixelSplat_fisheye
+
+This is a (very slightly modified) copy of **[pixelSplat:](https://github.com/dcharatan/pixelsplat) 3D Gaussian Splats from Image Pairs for Scalable Generalizable 3D Reconstruction** by David Charatan, Sizhe Li, Andrea Tagliasacchi, and Vincent Sitzmann.
+
+The goal of this project is to test pixelSplat on the fisheye images of the [KITTI-360 dataset](https://www.cvlibs.net/datasets/kitti-360/). A code is added to rectify the fisheye images of KITTI-360 as well as to transform the fisheye images, intrinsics and extrinsics to the format pixelSplat uses in its Real Estate 10k dataloader.
+
+For the necessary library installations and information about datasets, training, evaluation, etc. please visit the [GitHub page](https://github.com/dcharatan/pixelsplat) of pixelSplat.
+
+
+## BibTeX
+
+```
+@inproceedings{charatan23pixelsplat,
+      title={pixelSplat: 3D Gaussian Splats from Image Pairs for Scalable Generalizable 3D Reconstruction},
+      author={David Charatan and Sizhe Li and Andrea Tagliasacchi and Vincent Sitzmann},
+      year={2023},
+      booktitle={arXiv},
+}
+```
--- run.sh
+++ run.sh
@@ -11,14 +11,44 @@ python3 -m src.main +experiment=kitti360 mode=test exp_name='test_kitti360_debug
 # Test on Kitti360 with pre-trained weights
 python3 -m src.main +experiment=kitti360 mode=test exp_name='test_kitti360' hydra.run.dir='outputs/test_kitti360' dataset/view_sampler=evaluation checkpointing.load=pretrained_models/acid.ckpt dataset.view_sampler.index_path=assets/evaluation_index_kitti360.json test.output_path=outputs/test_kitti360 dataset.roots=[datasets/kitti360/test_images_cam02_0_249] 
 
+#### Breaking down the test command:
+# python3 -m src.main
+# +experiment=kitti360 
+# mode=test 
+# exp_name='test_kitti360'
+# hydra.run.dir='outputs/test_kitti360' 
+# dataset/view_sampler=evaluation 
+# checkpointing.load=pretrained_models/acid.ckpt 
+# dataset.view_sampler.index_path=assets/evaluation_index_kitti360.json 
+# test.output_path=outputs/test_kitti360 
+# dataset.roots=[datasets/kitti360/test_images_cam02_0_249] 
 
-+experiment=kitti360 
-mode=test 
-exp_name='test_kitti360'
-hydra.run.dir='outputs/test_kitti360' 
-dataset/view_sampler=evaluation 
-checkpointing.load=pretrained_models/acid.ckpt 
-dataset.view_sampler.index_path=assets/evaluation_index_kitti360.json 
-test.output_path=outputs/test_kitti360 
-dataset.roots=[datasets/kitti360/test_images_cam02_0_249] 
 
+
+# Calculate metrics
+
+# PixelSplat on ACID
+python3 -m src.scripts.compute_metrics +experiment=acid +evaluation=acid output_metrics_path=outputs/test_acid/test/acid/evaluation_metrics.json evaluation.methods.0.path=outputs/test_acid/test/acid
+
+# PixelSplat on KITTI360 debug
+python3 -m src.scripts.compute_metrics +experiment=kitti360 +evaluation=kitti360_debug dataset.roots=[datasets/kitti360] output_metrics_path=outputs/test_kitti360_debug/kitti360/evaluation_metrics.json evaluation.methods.0.path=outputs/test_kitti360_debug/kitti360
+
+# PixelSplat on KITTI360
+python3 -m src.scripts.compute_metrics +experiment=kitti360 +evaluation=kitti360 dataset.roots=[datasets/kitti360/test_images_rectified_0_249] output_metrics_path=outputs/test_kitti360/kitti360/evaluation_metrics.json evaluation.methods.0.path=outputs/test_kitti360/kitti360
+
+#### Breaking down the calculate metrics command:
+# python3 -m src.scripts.compute_metrics 
+# +experiment=kitti360 
+# +evaluation=kitti360 
+# dataset.roots=[datasets/kitti360/test_images_rectified_0_249] 
+# output_metrics_path=outputs/test_kitti360/kitti360/evaluation_metrics.json 
+# evaluation.methods.0.path=outputs/test_kitti360/kitti360
+
+
+
+
+# Estimated rectified fisheye camera parameters by NerfStudio
+"fl_x": 947.5964796581311   / 700
+"fl_y": 440.9180126120826  / 700
+"cx": 216.2577815920706   / 700
+"cy": 335.75905987919214  / 700
